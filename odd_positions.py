2b8c7296cfda9eac4c605786aea6e019790aae36
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2023-01-18 03:03:18 +0000

    added code

---
+++ odd_positions.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# Created by: Kaitlyn Ip
+# Created on: Jan 2023
+# This program prints the odd numbers in a list of 10 random numbers
+
+import random
+
+
+def find_odd_numbers(list_of_numbers):
+    # this function prints the odd numbers
+
+    odd_numbers = []
+    loop_counter = 1
+
+    for list_item in list_of_numbers:
+        if loop_counter % 2 != 0:
+            odd_numbers.append(list_item)
+        loop_counter += 1
+
+    return odd_numbers
+
+
+def main():
+    # this function creates 10 random numbers in a list
+
+    list_of_numbers = []
+
+    # input, process & output
+    n_num_of_elements = input("# of random numbers you want: ")
+
+    try:
+        num_of_elements = int(n_num_of_elements)
+        if num_of_elements > 0:
+            for loop_counter in range(0, num_of_elements):
+                random_num = random.randint(1, 100)  # a number between 1-100
+                list_of_numbers.append(random_num)
+                print(
+                    "The random number {0} is : {1}".format(
+                        loop_counter + 1, random_num
+                    )
+                )
+                loop_counter += 1
+
+            odd_numbers = find_odd_numbers(list_of_numbers)
+
+            print("\nThe numbers in odd positions in the list are : ", end="")
+            for list_item in odd_numbers:
+                print("{0} ".format(list_item), end="")
+        else:
+            print("\nThat is a negative input, please try again.")
+    except Exception:
+        print("\nThat is not a valid input, please try again.")
+
+    print("")
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
